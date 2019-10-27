21c3072e558348dc05665b924b6bc66e86e09205
ping58972 <ping58972@gmail.com>
2019-10-26 22:10:14 -0500

    Initializing Authentication by simple login page for beginning

---
+++ spring-security-demo/src/main/java/com/ping/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.ping.springsecurity.demo.config;
+
+import org.springframework.cglib.transform.impl.AddDelegateTransformer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// add out users for in memory authentication
+		
+		UserBuilder usersBuilder = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+		.withUser(usersBuilder.username("john").password("test123").roles("EMPLOYEE"))
+		.withUser(usersBuilder.username("mary").password("test123").roles("MANAGER"))
+		.withUser(usersBuilder.username("susan").password("test123").roles("EMPLOYEE"));
+	}
+
+}
